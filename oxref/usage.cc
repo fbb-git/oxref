a112fd3836c7052c3c957e8a0d02bfd2fa26b7e7
Frank B. Brokken <f.b.brokken@rug.nl>
2011-02-12 14:12:35 +0000

    wip
    git-svn-id: https://oxref.svn.sourceforge.net/svnroot/oxref/oxref/trunk@3 dcc949cb-2a1c-0410-b67a-e7e5ca830264

--- oxref/usage.cc
+++ oxref/usage.cc
@@ -11,8 +11,20 @@ void usage(std::string const &progname)
     "Usage: " << progname << " [options] args\n"
     "Where:\n"
     "   [options] - optional arguments (short options between parentheses):\n"
-    "      --help (-h)      - provide this help\n"
-    "      --version (-v)   - show version information and terminate\n"
+    
+    "      --arg (-a) ...\n"
+    "              count - display function's argument counts only\n"
+    "              <len> - restrict parameter names to the first <len> "
+                                                                "characters\n"
+    "      --help (-h)             - provide this help\n"
+    "      --no-object-files (-o)  - suppress the object file names\n"
+    "      --no-source-files (-s)  - suppress the source file names\n"
+    "      --select name           - only display the cross-reference of "
+                                                                "`name',\n"
+    "                                where `name' can be a substring of the "
+                                                            "function(s)\n"
+    "                                to display.\n"
+    "      --version (-v)          - show version information and terminate\n"
     "   args   - explain additional arguments.\n"
     "\n";
 }
